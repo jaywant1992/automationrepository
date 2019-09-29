2bb23a7acc25d83f5574681c459e042dcc835843
jaywant <jaywantingle@gmail.com>
2019-09-29 11:21:14 +0530

    Code content first java project

---
+++ basicCoreJava/src/DataTypesAndVariables/FirstJavaClass.java
@@ -0,0 +1,11 @@
+package DataTypesAndVariables;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome To Selenium");
+	}
+
+}
